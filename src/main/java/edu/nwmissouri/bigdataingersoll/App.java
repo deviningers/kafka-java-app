b1f330be442f78f57ba1ec2ae59e948a5d1b77af
Devin <deviningers@gmail.com>
2021-02-27 17:41:59 -0600

    got pokeAPI working

--- src/main/java/edu/nwmissouri/bigdataingersoll/App.java
+++ src/main/java/edu/nwmissouri/bigdataingersoll/App.java
@@ -1,13 +1,48 @@
 package edu.nwmissouri.bigdataingersoll;
 
+import java.util.Scanner;
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import me.sargunvohra.lib.pokekotlin.client.PokeApi;
+import me.sargunvohra.lib.pokekotlin.client.PokeApiClient;
+
 /**
- * Hello world!
- *
+ * Author: Devin Ingersoll
+ * Description: Kafka producer that 
+ * prints out pokemon name by number
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App  {  
+    private static Scanner in;
+    public static void main(String[] argv)throws Exception {
+        if (argv.length != 1) {
+            System.err.println("Please specify 1 parameters ");
+            System.exit(-1);
+        }
+
+        PokeApi pokeApi = new PokeApiClient();
+
+        String topicName = argv[0];
+        in = new Scanner(System.in);
+        System.out.println("Enter message(type exit to quit)");
+
+        //Configure the Producer
+        Properties configProperties = new Properties();
+        configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.ByteArraySerializer");
+        configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
+
+        org.apache.kafka.clients.producer.Producer producer = new KafkaProducer(configProperties);
+        String line = in.nextLine();
+        while(!line.equals("exit")) {
+            ProducerRecord<String, String> rec = new ProducerRecord<String, String>(topicName,line);
+            producer.send(rec);
+            line = in.nextLine();
+        }
+        in.close();
+        producer.close();
     }
 }
